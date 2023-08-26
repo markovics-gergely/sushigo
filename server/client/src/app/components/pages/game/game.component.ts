99460fddd6e639107deaae025224022b45062faf
Markovics Gergely <gergely.markovics@telcotrend.hu>
2023-08-26 13:23:51 +0200

    s

--- server/client/src/app/components/pages/game/game.component.ts
+++ server/client/src/app/components/pages/game/game.component.ts
@@ -1,40 +1,57 @@
 import { Component } from '@angular/core';
 import { GameService } from 'src/app/services/game.service';
+import { TokenService } from 'src/app/services/token.service';
+import { UserService } from 'src/app/services/user.service';
 import { DeckType } from 'src/shared/deck.models';
-import { IGameViewModel, Phase } from 'src/shared/game.models';
+import { IBoardViewModel, IGameViewModel, Phase } from 'src/shared/game.models';
+import { IUserViewModel } from 'src/shared/user.models';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
 })
 export class GameComponent {
   private _game: IGameViewModel | undefined = {
     id: '',
     name: '',
-    players: Array.from({ length: 10 }, () =>
-      ({
-        id: '',
-        userId: '',
-        userName: 'name',
-        points: 0,
-        board: { cards: [] },
-        handId: ''
-      })
-    ),
+    players: Array.from({ length: 10 }, () => ({
+      id: '',
+      userId: '',
+      userName: 'name',
+      points: 0,
+      board: { cards: [] },
+      handId: '',
+    })),
     deckType: DeckType.SushiGo,
     round: 0,
     actualPlayerId: '',
     firstPlayerId: '',
     phase: Phase.Turn,
   };
-  public get game(): IGameViewModel | undefined { return this._game; }
+  public get game(): IGameViewModel | undefined {
+    return this._game;
+  }
 
   constructor(
-    private gameService: GameService
+    private gameService: GameService,
+    private tokenService: TokenService,
   ) {
     gameService.gameEventEmitter.subscribe((game: IGameViewModel | undefined) => {
       this._game = game;
     });
+  }
+
+  public get isMyTurn(): boolean {
+    return (
+      this.game?.actualPlayerId === this.tokenService.user?.sub &&
+      this.game?.phase === Phase.Turn
+    );
+  }
+
+  public get board(): IBoardViewModel | undefined {
+    return this.game?.players.find(
+      (player) => player.userId === this.tokenService.user?.sub
+    )?.board;
   }
 }
--- server/client/src/app/components/pages/game/hand/hand.component.ts
+++ server/client/src/app/components/pages/game/hand/hand.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CardService } from 'src/app/services/card.service';
-import { IHandViewModel } from 'src/shared/game.models';
+import { IBoardViewModel, IHandViewModel } from 'src/shared/game.models';
 
 @Component({
   selector: 'app-hand',
@@ -14,6 +14,8 @@ export class HandComponent {
   }
 
   protected selectedHand = ["hand"];
+
+  @Input() board: IBoardViewModel | undefined;
 
   constructor(private cardService: CardService) {
     this.cardService.handEventEmitter.subscribe((hand: IHandViewModel | undefined) => {
